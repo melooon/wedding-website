7983b81059dd58b239e4f0b3055168a1b1d2eda0
melon <hugo.meloni@gmail.com>
2020-01-04 21:23:56 +0400

    Fixing countdown

--- js/main.js
+++ js/main.js
@@ -274,7 +274,7 @@
 			var timeinterval = setInterval(updateClock, 1000);
 		}
 		// set your wedding date here
-		var deadline = 'November 28 2019 17:30:00 GMT+0300';
+		var deadline = 'August 22 2019 17:30:00 GMT+0200';
 		if (countdown){
 			initializeClock('timer', deadline);
 		}
